4b3f3b20f59fdcd876b6003bd108fd87db8cc67f
atehortuua <matehortua32@gmail.com>
2025-09-24 11:34:44 -0500

    se cambia el nombre de la ruta

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import { createProduct, createUsers, deleteProduct, deleteUser, getproduct, getUsers, updateProduct, updateUser } from '../controllers/users.controllers.js';
-const router = express.Router();//permite crear las rutas
+const routerProduct = express.Router();//permite crear las rutas
 
 
-router.get("/product", getproduct);
-router.post("/createProduct",  createProduct)
-router.put("/updateProduct", updateProduct)
-router.delete("/deleteProduct", deleteProduct)
+routerProduct.get("/product", getproduct);
+routerProduct.post("/createProduct",  createProduct)
+routerProduct.put("/updateProduct", updateProduct)
+routerProduct.delete("/deleteProduct", deleteProduct)
+
+
+export default router 
