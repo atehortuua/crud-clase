6589d90cc9dfe67703606a993a1226a6a592df86
atehortuua <matehortua32@gmail.com>
2025-09-23 19:53:51 -0500

    coneccion a la base de datos

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //importare express
 //escuchar puerto cors 
 import express from 'express'; //module = javascript
+import { connect } from 'mongoose';
+import { connectDB } from './src/config/db-config.js';
 // const express = requiere('express');//commonJs= node js
 
 const app = express();
 
-
+connectDB();
 
 app.listen(3000, ()=>{
     console.log(`servidor corriendo en el puerto 3000`)
