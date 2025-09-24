304c9c3e00008da0796995a71674212041a787f8
atehortuua <matehortua32@gmail.com>
2025-09-23 20:39:08 -0500

    create .env - .gitgnore -env-example

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //importare express
 //escuchar puerto cors 
 import express from 'express'; //module = javascript
-import { connect } from 'mongoose';
 import { connectDB } from './src/config/db-config.js';
 // const express = requiere('express');//commonJs= node js
-
+import { configDotenv } from 'dotenv';
+configDotenv( )
 const app = express();
 
 connectDB();
-
-app.listen(3000, ()=>{
-    console.log(`servidor corriendo en el puerto 3000`)
+const port = process.env.PORT
+app.listen(port, ()=>{
+    console.log(`servidor corriendo en el puerto ${ port}`)
 });
